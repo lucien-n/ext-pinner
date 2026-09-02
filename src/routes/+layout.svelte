a657fd864e5445d8403b55b3d930c317a675b6e2
Lucien <lucien.neuhoff@protonmail.com>
2026-09-02 10:12:42 +0200

    added app icon header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,13 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="flex h-[400] w-md min-w-md flex-col bg-background p-2">
-	<ScrollArea type="scroll" class="h-full p-3">
+<div class="flex h-[400] w-md min-w-md flex-col gap-1 bg-background p-2">
+	<div class="items-ce flex gap-2 px-5">
+		<img src="icon.svg" alt="Pinner icon" class="w-4" />
+		<h1 class="text-md font-semibold">Pinner</h1>
+	</div>
+
+	<ScrollArea type="scroll" class="h-full px-3 pb-3">
 		{@render children()}
 	</ScrollArea>
 </div>
