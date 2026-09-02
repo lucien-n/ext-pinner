cae0aba9e6a81cc0af490511302f9cd72f375d8b
Lucien <lucien.neuhoff@protonmail.com>
2026-09-02 10:16:34 +0200

    reduced padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,12 +14,12 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <div class="flex h-[400] w-md min-w-md flex-col gap-1 bg-background p-2">
-	<div class="items-ce flex gap-2 px-5">
+	<div class="flex items-center gap-2 px-5 pt-1">
 		<img src="icon.svg" alt="Pinner icon" class="w-4" />
-		<h1 class="text-md font-semibold">Pinner</h1>
+		<h1 class="text-lg font-semibold">Pinner</h1>
 	</div>
 
-	<ScrollArea type="scroll" class="h-full px-3 pb-3">
+	<ScrollArea type="scroll" class="h-full p-3 pt-2">
 		{@render children()}
 	</ScrollArea>
 </div>
