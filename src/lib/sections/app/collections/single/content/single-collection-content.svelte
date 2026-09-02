df706db8ce0129bc7a4010b032bd6e75e3c08d69
Lucien <lucien.neuhoff@protonmail.com>
2026-09-02 10:29:12 +0200

    show hostname by default

--- src/lib/sections/app/collections/single/content/single-collection-content.svelte
+++ src/lib/sections/app/collections/single/content/single-collection-content.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { Button } from '&/button';
+	import { Button, buttonVariants } from '&/button';
+	import { Checkbox } from '&/checkbox';
+	import { Label } from '&/label';
 	import DragIcon from '@lucide/svelte/icons/grip-vertical';
 	import RemoveIcon from '@lucide/svelte/icons/x';
 	import { dragHandle, dragHandleZone, type DndEvent } from 'svelte-dnd-action';
@@ -8,6 +10,8 @@
 	import AddUrlAction from './add-url-action.svelte';
 
 	const ctx = getPinnerCollectionCtx();
+
+	let shouldShowFullUrls = $state(false);
 
 	let items = $derived(ctx.data.urls.map((url) => ({ id: url, url })));
 
@@ -21,9 +25,16 @@
 	}
 </script>
 
-<p class="text-sm font-medium">
-	{ctx.data.urls.length} tab{ctx.data.urls.length > 1 ? 's' : ''}
-</p>
+<div class="flex items-center gap-1">
+	<p class="text-sm font-medium">
+		{ctx.data.urls.length} tab{ctx.data.urls.length > 1 ? 's' : ''}
+	</p>
+
+	<Label class={buttonVariants({ size: 'xs', variant: 'ghost' })}>
+		Show full
+		<Checkbox bind:checked={shouldShowFullUrls} />
+	</Label>
+</div>
 
 <div
 	use:dragHandleZone={{
@@ -39,15 +50,28 @@
 	class="flex flex-col gap-1"
 >
 	{#each items as item (item.id)}
+		{@const hostname = new URL(item.url).hostname}
 		<div class="flex min-w-0 items-center justify-between" animate:flip={{ duration: 150 }}>
-			<Button
-				variant="link"
-				href={item.url}
-				target="_blank"
-				class="block min-w-0 flex-1 overflow-hidden px-0 text-left font-normal text-ellipsis whitespace-nowrap"
-			>
-				{item.url}
-			</Button>
+			<div class="flex gap-1">
+				<img
+					src="https://www.google.com/s2/favicons?domain={hostname}&sz=32"
+					alt="{hostname}'s icon"
+					class="mt-1 size-4"
+				/>
+
+				<Button
+					variant="link"
+					href={item.url}
+					target="_blank"
+					class="block min-w-0 flex-1 overflow-hidden px-0 py-0 text-left font-normal text-ellipsis whitespace-nowrap"
+				>
+					{#if shouldShowFullUrls}
+						{item.url}
+					{:else}
+						{hostname}
+					{/if}
+				</Button>
+			</div>
 
 			<div class="flex shrink-0 items-center gap-1">
 				<Button
