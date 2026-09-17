3ef4f95542acb3d60e11a128ceb69984e3166a13
Lucien <lucien.neuhoff@protonmail.com>
2026-09-17 10:09:31 +0200

    fix: NODE_ENV

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter({
-				manifestFile: process.env.NODE_ENV === 'prod' ? 'manifest.json' : 'manifest-dev.json'
+				manifestFile: process.env.NODE_ENV === 'production' ? 'manifest.json' : 'manifest-dev.json'
 			}),
 			appDir: 'app',
 			alias: { '&': './src/lib/shadcn/ui' }
